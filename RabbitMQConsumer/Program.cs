e913a21eae23c3728ed84078f40f65853d55e7fa
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2023-09-29 13:52:04 +0300

    Add project files.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Thread.Sleep(10000);
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://vvjjdmze:D-T54SjsLmo6O0DG0XO1XoU32-N_o90E@puffin.rmq2.cloudamqp.com/vvjjdmze");
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("FristTutorialRabbitmq", false, false, true);
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume("FristTutorialRabbitmq", false, consumer);
+    consumer.Received += (sender, e) =>
+    {
+        //e.Body : Kuyruktaki mesajı verir.
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+    };
+}
+Console.Read();
---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://vvjjdmze:D-T54SjsLmo6O0DG0XO1XoU32-N_o90E@puffin.rmq2.cloudamqp.com/vvjjdmze");
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("FristTutorialRabbitmq", false, false, true);
+    byte[] bytemessage = Encoding.UTF8.GetBytes("Frist Tutorial On Rabbitmq test");
+    channel.BasicPublish(exchange: "", routingKey: "FristTutorialRabbitmq", body: bytemessage);
+}
